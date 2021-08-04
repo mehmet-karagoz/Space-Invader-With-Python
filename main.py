631ec25a88a52ffc9021f9916a2cc8b1556d33d8
mehmet-karagoz <atleownhem@gmail.com>
2021-08-04 13:57:08 +0300

    added game screen and loop

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ import pygame
 
 pygame.init()
 
+#create screen
+screen = pygame.display.set_mode((800, 600))
+
+#game loop
+run = True
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
 pygame.quit()
